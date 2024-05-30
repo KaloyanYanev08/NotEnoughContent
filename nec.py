e39bb6e03d3d1064ba591c6637e0af633f7e7877
Kal <135052657+KaloyanYanev08@users.noreply.github.com>
2024-05-30 21:14:36 +0300

    Article database
    Needs functional article creation for paragraph creation test
    
    Need to fix sqlalchemy.exc.StatementError: (builtins.TypeError) memoryview: a bytes-like object is required, not 'FileStorage'
    [SQL: INSERT INTO article (title, picture_data, access_id) VALUES (?, ?, ?)]

--- nec.py
+++ nec.py
@@ -1,29 +1,96 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_admin import Admin, form
+from flask_admin.contrib.sqla import ModelView
+from wtforms import FileField, TextAreaField, SelectField
 
-#Initialize app and SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SECRET_KEY'] = 'femboysAreHot'
 db = SQLAlchemy(app)
 
-#Define models (will change soon)
-class News(db.Model):
+class Access(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.now)
+    name = db.Column(db.String(20), nullable=False)
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    picture_data = db.Column(db.LargeBinary, nullable=False)
+    access_id = db.Column(db.Integer, db.ForeignKey('access.id'), nullable=False)
+    paragraphs = db.relationship('Paragraph', backref='article', lazy=True)
 
-#Create the database tables
+class Paragraph(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    article_id = db.Column(db.Integer, db.ForeignKey('article.id'), nullable=False)
+
+# Command: Save picture data
+def save_picture_data(article, picture):
+    if picture:
+        picture_data = picture.read()
+        article.picture_data = picture_data
+
+
+
+# Query: Get all access options
+def get_all_access_options():
+    return Access.query.all()
+
+# Query: Get article by ID
+def get_article_by_id(article_id):
+    return Article.query.get(article_id)
+
+# Create the database tables
 with app.app_context():
     db.create_all()
 
-#routes (links)
+# Initialize Flask-Admin
+admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
+
+class ArticleForm(form.BaseForm):
+        description_id = TextAreaField('Description')
+        access_id = SelectField('Access')
+
+class ArticleView(ModelView):
+    form_extra_fields = {
+        'picture_data': FileField('Picture')
+    }
+    form_base_class = ArticleForm
+    column_list = ('title', 'picture_data', 'description_id', 'access_id')
+
+    
+
+    def create_form(self, obj=None):
+        form = super().create_form(obj)
+        form.access_id.choices = [(a.id, a.name) for a in get_all_access_options()]
+        return form
+
+admin.add_view(ArticleView(Article, db.session))
+admin.add_view(ModelView(Access, db.session))
+admin.add_view(ModelView(Paragraph, db.session))
+
+# Routes
 @app.route('/')
 def index():
-    return render_template('index.html')
+    articles = Article.query.all()
+    return render_template('index.html', articles=articles)
+
+@app.route('/create_article', methods=['POST'])
+def create_article():
+    title = request.form['title']
+    access_id = request.form['access_id']
+    picture = request.files['picture_data']
+
+    article = Article(title=title, access_id=access_id)
+
+    save_picture_data(article, picture)
+
+    db.session.add(article)
+    db.session.commit()
+
+    return 'Article created successfully!'
 
 #Run
 if __name__ == "__main__":
