b9ab984460feccf4a75dc4926deea73760a9ac99
Kal <135052657+KaloyanYanev08@users.noreply.github.com>
2024-06-04 22:09:36 +0300

    App before adding security

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
-from nec import db
-from nec import app
+from nec import db, app, create_superuser, create_default_accesses
 
 with app.app_context():
     db.drop_all()
     db.create_all()
+    create_default_accesses()
--- nec.py
+++ nec.py
@@ -4,6 +4,10 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from wtforms import FileField, TextAreaField, SelectField, StringField, Form, FieldList, FormField
 import base64
+from werkzeug.security import generate_password_hash
+from getpass import getpass
+import sys
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -11,9 +15,43 @@ app.config['SECRET_KEY'] = 'femboysAreHot'
 
 db = SQLAlchemy(app)
 
+#class Role(db.Model, RoleMixin):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(20), unique=True, nullable=False)
+
+
 class Access(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
+
+class ProfileImg(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    mimetype = db.Column(db.String(100))
+    img_base64 = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, name, mimetype, img_base64, user_id):
+        self.name = name
+        self.mimetype = mimetype
+        self.img_base64 = img_base64
+        self.user_id = user_id
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    nickname = db.Column(db.String(25), nullable=True)
+    role = db.Column(db.Integer, db.ForeignKey('access.id'), nullable=False, default=1)
+    superuser = db.Column(db.Boolean, default=False)
+
+    def __init__(self, username, email, password, superuser=False, role=1):
+        self.username = username
+        self.email = email
+        self.password = password
+        self.role = role
+        self.superuser = superuser
 
 class Img(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -61,9 +99,33 @@ def get_all_access_options():
 def get_article_by_id(article_id):
     return Article.query.get(article_id)
 
+def create_superuser(username, email, password):
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        print("User already exists.")
+        return
+
+    hashed_password = generate_password_hash(password)
+
+    superuser = User(username=username, email=email, password=hashed_password, superuser=True, role=3)
+    db.session.add(superuser)
+    db.session.commit()
+
+    print("Superuser created successfully.")
+
+def create_default_accesses():
+    accesses = [
+        Access(name="User"),
+        Access(name="Team"),
+        Access(name="Owner")
+    ]
+    db.session.bulk_save_objects(accesses)
+    db.session.commit()
+
 with app.app_context():
     db.drop_all()
     db.create_all()
+    create_default_accesses()
 
 admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
 
@@ -100,9 +162,31 @@ class ArticleView(ModelView):
             article_id = model.id
             save_picture_data(article_id, picture)
 
+class UserView(ModelView):
+        column_list = ['username', 'email', 'nickname', 'role', 'superuser']
+        form_columns = ['username', 'email', 'password', 'nickname', 'role', 'superuser']
+        form_extra_fields = {
+            'password': StringField('Password'),
+        }
+
+class ImgView(ModelView):
+    column_list = ['name', 'mimetype', 'article_id']
+    form_columns = ['name', 'mimetype', 'img_base64', 'article_id']
+    can_view_details = True
+    column_details_list = ['name', 'mimetype', 'img_base64', 'article_id']
+
+class ProfileImgView(ModelView):
+    column_list = ['name', 'mimetype', 'user_id']
+    form_columns = ['name', 'mimetype', 'img_base64', 'user_id']
+    can_view_details = True
+    column_details_list = ['name', 'mimetype', 'img_base64', 'user_id']
+
+admin.add_view(UserView(User, db.session))
 admin.add_view(ArticleView(Article, db.session))
 admin.add_view(ModelView(Access, db.session))
 admin.add_view(ModelView(Paragraph, db.session))
+admin.add_view(ImgView(Img, db.session))
+admin.add_view(ProfileImgView(ProfileImg, db.session))
 
 @app.route('/')
 def index():
@@ -146,7 +230,6 @@ def create_article():
     else:
         access_options = get_all_access_options()
         return render_template('create_article.html', access_options=access_options)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
